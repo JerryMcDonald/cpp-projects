6b250b5700812286985424b2bc9f9474be2bf374
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2020-12-24 20:58:22 -0600

    std namespace

--- OOYA/profile.hpp
+++ OOYA/profile.hpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
 
 class Profile {
 private:
-  std::string name;
+  string name;
   int age;
-  std::string city;
-  std::string country;
-  std::string pronouns;
-  std::vector<std::string> hobbies;
+  string city;
+  string country;
+  string pronouns;
+  vector<string> hobbies;
   
 public:
-  Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
-  std::string view_profile();
-  void add_hobby(std::string new_hobby);
+  Profile(string new_name, int new_age, string new_city, string new_country, string new_pronouns = "they/them");
+  string view_profile();
+  void add_hobby(string new_hobby);
 
 };
