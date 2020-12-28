dfde789b98fc60ab720566ab9ef8c03ec5e4b09e
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2020-12-27 22:58:38 -0600

    spacing

--- TTT/play.cpp
+++ TTT/play.cpp
@@ -34,35 +34,7 @@ void introduction() {
 
 }
 
-bool is_winner() {
 
-  bool winner = false;
-  // rows
-  if ((board[0] == board[1]) && (board[1] == board[2]) && board[0] != " ") {
-    winner = true;
-  } else if ((board[3] == board[4]) && (board[3] == board[5]) && board[3] != " ") {
-    winner = true;
-  } else if ((board[6] == board[7]) && (board[6] == board[8]) && board[6] != " ") {
-    winner = true;
-  } 
-  // columns
-  else if ((board[0] == board[3]) && (board[0] == board[6]) && board[0] != " ") {
-    winner = true;
-  } else if ((board[1] == board[4]) && (board[1] == board[7]) && board[1] != " ") {
-    winner = true;
-  } else if ((board[2] == board[5]) && (board[2] == board[8]) && board[2] != " ") {
-    winner = true;
-  } // diagonals
-  else if ((board[0] == board[4]) && (board[0] == board[8]) && board[0] != " ") {
-    winner = true;
-  }
-  else if ((board[2] == board[4]) && (board[2] == board[6]) && board[2] != " ") {
-    winner = true;
-  }
-
-  return winner;
-
-}
 
 bool filled_up() {
 
@@ -96,6 +68,7 @@ void draw() {
   cout << "     |     |      \n";
 
   cout << "  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << "\n";
+  
   cout << "     |     |      \n";
 
   cout << "\n";
